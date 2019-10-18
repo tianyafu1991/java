2bd4fe2a596cd7b895c7c404f1f58ad4db3542c5
tianyafu1991 <1193835093@qq.com>
2019-10-18 11:36:54 +0800

    feat:添加final 和 权限访问控制符 的部分资料

---
+++ java_basic/src/main/java/com/tianyafu/java/basic/oop_2/final_7/Main.java
@@ -0,0 +1,19 @@
+package com.tianyafu.java.basic.oop_2.final_7;
+
+/**
+ * final关键字
+ * 被 final关键字修饰的类 不能被继承
+ * 被 final关键字修饰的变量 初始化之后不能被修改
+ * 被 final关键字修饰的方法 不能被子类重写
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        final int a = 10;
+        //被 final关键字修饰的变量 初始化之后不能被修改
+//        a = 20;
+
+    }
+}
---
+++ java_basic/src/main/java/com/tianyafu/java/basic/oop_2/public_protected_private_default_8/Main.java
@@ -0,0 +1,15 @@
+package com.tianyafu.java.basic.oop_2.public_protected_private_default_8;
+
+/**
+ * 权限修饰符
+ * public 在所有类中都可以访问
+ * protected  本类、子类、同包类
+ * default 如果是default的 不需要显式的写出是default的 ，可以在本类和同包类中访问
+ * private 只能在本类中访问  一般属性需要私有化 然后提供public的方法来访问属性
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
